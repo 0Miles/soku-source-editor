954f37d20c3cc2c5a0d2324c5f434a775887740d
Miles <git@miles.tw>
2023-11-18 01:33:15 +0800

    style

--- renderer/app.jsx
+++ renderer/app.jsx
@@ -32,7 +32,7 @@ export default function App() {
                 <div className="flex flex:row h:full">
                     <div className="flex h:full flex:col user-select:none>*">
                         <Button onClick={() => toggleSidebar()} appearance="transparent" className="translate(10,16) hide!@sm" icon={menuIcon}></Button>
-                        <div className={`flex flex:col h:full min-w:310 max-w:310 p:16 mr:-16 bg:#202020@dark bg:white@light ml:0 z:999 abs@<sm ~margin-left|.15s|ease-in ${showSidebar ? 'ml:0 box-shadow:0|0|5|3|gray/.3@<sm@light box-shadow:0|0|5|3|black/.3@<sm@dark' : 'ml:-310@<sm'}`}>
+                        <div className={`flex flex:col h:full min-w:310 max-w:310 p:16 mr:-16 bg:#202020@dark bg:white@light ml:0 z:999 {view-transition-name:sidebar} abs@<sm ~margin-left|.15s|ease-in ${showSidebar ? 'ml:0 box-shadow:0|0|5|3|gray/.3@<sm@light box-shadow:0|0|5|3|black/.3@<sm@dark' : 'ml:-310@<sm'}`}>
                             <Sidebar onTabChange={() => toggleSidebar(false)} />
                         </div>
                     </div>
@@ -45,7 +45,7 @@ export default function App() {
                     </div>
                     <div className="flex flex:col flex:1 w:0 align-items:center">
                         <div className="flex flex:col flex:1 w:full max-w:960 overflow:clip">
-                            <div className="w:full px:24 pt:8 pb:16">
+                            <div className="w:full px:24 pt:8 pb:16 z-index:1 bg:#202020@dark bg:white@light {view-transition-name:breadcrumbs}">
                                 <Breadcrumbs />
                             </div>
                             <Outlet />
--- renderer/templates/page-container.jsx
+++ renderer/templates/page-container.jsx
@@ -5,13 +5,6 @@ export default function PageContainer({ children, className }) {
                     h:full 
                     px:24 pb:24 pr:42@<sm 
                     overflow-y:auto
-                    hide::-webkit-scrollbar-button
-                    {bg:white;w:12}::-webkit-scrollbar@light
-                    {bg:white}::-webkit-scrollbar-track@light
-                    {bg:#babac0;border-radius:12;b:4|solid|white}::-webkit-scrollbar-thumb@light
-                    {bg:#202020;w:12;abs}::-webkit-scrollbar@dark
-                    {bg:#202020;abs}::-webkit-scrollbar-track@dark
-                    {bg:#959595;border-radius:12;b:4|solid|#202020;abs}::-webkit-scrollbar-thumb@dark
                 `}>
             {children}
         </div>)
