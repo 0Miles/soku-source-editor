7b4c52b5206abe179fdb02d077c398b02faaf944
Miles <git@miles.tw>
2024-09-08 21:09:10 +0800

    Refactor IPC module-related files

--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,160 @@
+import path from 'node:path'
 import { app, BrowserWindow, ipcMain, nativeTheme, dialog } from 'electron'
 import { autoUpdater } from 'electron-updater'
 import { createMainWindow } from './window/main.window'
 
 import { ConfigManager } from './config-manager'
-const configManager = new ConfigManager()
-
 import { SourceManager } from './source-manager/source-manager'
+import { getFileTree } from './utils/get-file-tree'
+
+import type { ModuleRepository } from './source-manager/types/module-repository'
+import type { ModuleInfo } from './source-manager/types/module-info'
+import type { ModuleVersionInfo } from './source-manager/types/module-version-info'
+import type { ModuleVersionDownloadLink } from './source-manager/types/module-version-download-link'
+
+const configManager = new ConfigManager()
 const sourceManager = new SourceManager()
 
-import configIniter from './ipc/config'
-import themeIniter from './ipc/theme'
-import modIniter from './ipc/mod'
-import gitIniter from './ipc/git'
+// config
+ipcMain.handle('get-config', (_, key) => {
+    return configManager.getConfig(key)
+})
 
+ipcMain.handle('update-config', (_, patch) => {
+    configManager.updateConfig(patch)
+})
+
+// theme
 nativeTheme.themeSource = configManager.getConfig('theme') ?? 'system'
+ipcMain.handle('switch-native-theme', (_, message) => {
+    if (['dark', 'light', 'system'].includes(message)) {
+        nativeTheme.themeSource = message
+    }
+})
 
-configIniter.initHandle(configManager)
-themeIniter.initHandle()
-modIniter.initHandle(sourceManager)
-gitIniter.initHandle(sourceManager, configManager)
+// mod
+ipcMain.handle('get-mods', (_, sourceName: string) => {
+    return sourceManager.getSource(sourceName)?.modules?.map(x => x.getData())
+})
+
+ipcMain.handle('get-mod', (_, sourceName: string, modName: string) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.getData()
+})
+
+ipcMain.handle('get-mod-versions', (_, sourceName: string, modName: string) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.versions?.map(x => x?.getData())
+})
+
+ipcMain.handle('get-sources', () => {
+    return sourceManager.sources?.map(x => x.getData())
+})
+
+ipcMain.handle('add-mod', (_, sourceName: string, modName: string, modInfo) => {
+    return sourceManager.getSource(sourceName)?.addModule(modName, modInfo)
+})
+
+ipcMain.handle('add-mod-version', (_, sourceName: string, modName: string, version: string, versionInfo: ModuleVersionInfo) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.addVersion(version, versionInfo)
+})
+
+ipcMain.handle('add-mod-version-download-link', (_, sourceName: string, modName: string, version: string, downloadLink: ModuleVersionDownloadLink) => {
+    const versionEntry = sourceManager?.getSource(sourceName)?.getModule(modName)?.getVersion(version)
+    const downloadLinks = versionEntry?.element.info?.downloadLinks ?? []
+    if (!downloadLinks.find(x => x.type === downloadLink.type && x.url === downloadLink.url)) {
+        downloadLinks.push(downloadLink)
+        versionEntry?.element.updateInfo({ downloadLinks })
+    }
+})
+
+ipcMain.handle('copy-mod-icon-file', (_, sourceName: string, modName: string, icon: any) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.copyAndReplaceImage(icon, 'icon')
+})
+
+ipcMain.handle('copy-mod-banner-file', (_, sourceName: string, modName: string, banner: any) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.copyAndReplaceImage(banner, 'banner')
+})
+
+ipcMain.handle('update-mod', (_, sourceName: string, modName: string, modInfo: ModuleInfo) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.update(modInfo)
+})
+
+ipcMain.handle('update-mod-version', (_, sourceName: string, modName: string, version: string, versionInfo: ModuleVersionInfo) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.getVersion(version)?.update(versionInfo)
+})
+
+ipcMain.handle('delete-source', (_, sourceName: string) => {
+    return sourceManager.deleteSource(sourceName)
+})
+
+ipcMain.handle('delete-mod', (_, sourceName: string, modName: string) => {
+    return sourceManager.getSource(sourceName)?.deleteModule(modName)
+})
+
+ipcMain.handle('delete-mod-version', (_, sourceName: string, modName: string, version: string) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.deleteVersion(version)
+})
+
+ipcMain.handle('copy-mod-version-files', (_, sourceName: string, modName: string, version: string, files: any[]) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.getVersion(version)?.copyModVersionFiles(files)
+})
+
+ipcMain.handle('export-zip', async (_, sourceName: string, modName: string, version: string) => {
+    const window = BrowserWindow.getFocusedWindow()
+    if (window) {
+        const result = await dialog.showSaveDialog(window, {
+            defaultPath: `${modName}_${version}.zip`,
+            filters: [
+                { name: 'Zip Files', extensions: ['zip'] },
+                { name: 'All Files', extensions: ['*'] }
+            ]
+        })
+
+        if (!result.canceled) {
+            sourceManager
+                .getSource(sourceName)
+                ?.getModule(modName)
+                ?.exportZip(version, path.dirname(result.filePath ?? ''), path.basename(result.filePath ?? ''))
+        }
+    }
+})
+
+ipcMain.handle('export-zip-to-output', (_, sourceName: string, modName: string, version: string) => {
+    return sourceManager.getSource(sourceName)?.getModule(modName)?.exportZipToOutput(version)
+})
+
+ipcMain.handle('get-file-tree', (_, paths: string | string[]) => {
+    if (!Array.isArray(paths)) {
+        paths = [paths]
+    }
+    return getFileTree(paths)
+})
+
+// git
+ipcMain.handle('add-github-release', async (_, sourceName: string, modName: string, version: string, repository: ModuleRepository) => {
+    return await sourceManager.getSource(sourceName)?.getModule(modName)?.createGithubTagAndRelease(version, repository, configManager.getConfig('githubToken'))
+})
+
+ipcMain.handle('add-gitee-release', async (_, sourceName: string, modName: string, version: string, repository: ModuleRepository) => {
+    return await sourceManager.getSource(sourceName)?.getModule(modName)?.createGiteeTagAndRelease(version, repository, configManager.getConfig('giteeToken'))
+})
+
+ipcMain.handle('clone-mod-source', async (_, url: string, customName: string) => {
+    return await sourceManager.addSource(url, customName)
+})
+
+ipcMain.handle('git-sync', async (_, sourceName: string, sourceBranch?: string) => {
+    await sourceManager.getSource(sourceName)?.sync(sourceBranch)
+    return JSON.parse(JSON.stringify(sourceManager.getSource(sourceName)?.status))
+})
+
+ipcMain.handle('git-fetch', async (_, sourceName: string) => {
+    await sourceManager.getSource(sourceName)?.fetchStatus()
+    return JSON.parse(JSON.stringify(sourceManager.getSource(sourceName)?.status))
+})
+
+ipcMain.handle('git-git-revert-changes', async (_, sourceName: string) => {
+    return await JSON.parse(JSON.stringify(sourceManager.getSource(sourceName)?.revertChanges()))
+})
 
 app.whenReady().then(() => {
     autoUpdater.checkForUpdatesAndNotify()
